39b3b226bed80647f62e57d6e52a0bc1a079fd9b
HongJunMo <wnsah717@daum.net>
2021-08-12 11:41:08 +0900

    tv기능 완성

--- src/com/care/def/MainClass.java
+++ src/com/care/def/MainClass.java
@@ -2,9 +2,12 @@ package com.care.def;
 
 import java.util.Scanner;
 
+import com.care.tv_hongjunmo.Tv_hongjunmo;
+
 public class MainClass {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		Tv_hongjunmo tv = new Tv_hongjunmo();
 		int num;
 		System.out.println("---가전제품---");
 		while(true) {
@@ -16,6 +19,7 @@ public class MainClass {
 			switch(num) {
 			case 1 :
 				System.out.println("냉장고");
+				tv.display();
 				break;	
 			case 2 :
 				System.out.println("Tv");
