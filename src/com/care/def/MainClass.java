8dbeaa6e4c70c488d2d6b90197f2393ba234843a
hjoo <oct20ray@gmail.com>
2021-08-12 11:41:16 +0900

    TV기능 완성

--- src/com/care/def/MainClass.java
+++ src/com/care/def/MainClass.java
@@ -2,19 +2,23 @@ package com.care.def;
 
 import java.util.Scanner;
 
+import com.care.tv_joohui.TV_joohui;
+
 public class MainClass {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		TV_joohui tv_joohui = new TV_joohui();
 		int num;
 		System.out.println("---∞°¿¸¡¶«∞---");
 		while(true) {
 			System.out.println("1.≥√¿Â∞Ì");
 			System.out.println("2.Tv");
-			System.out.println("3.ºº≈π±‚");
+			System.out.println("3.ºº≈π±‚");		
 			System.out.print(">>>> : ");
 			num = input.nextInt();
 			switch(num) {
 			case 1 :
+				tv_joohui.display();
 				System.out.println("≥√¿Â∞Ì");
 				break;	
 			case 2 :
